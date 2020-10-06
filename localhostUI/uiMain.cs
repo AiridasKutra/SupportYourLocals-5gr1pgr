f64025834fbcc86c7b1b237cfc1edb3722bc9728
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2020-10-06 13:54:28 +0300

    Added UI project. Named: "localhostUI".

---
+++ localhostUI/uiMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace localhostUI
+{
+    public partial class uiMain : Form
+    {
+        public uiMain()
+        {
+            InitializeComponent();
+        }
+
+        private void MainLoad(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
