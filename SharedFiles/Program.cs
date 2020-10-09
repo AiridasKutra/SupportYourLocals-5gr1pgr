3b1371b0ae12936392d32001cbd914e8c115414e
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-09 18:30:50 +0300

    Added logic to retrieve data from server for testing

--- SharedFiles/Program.cs
+++ SharedFiles/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common;
+using Common.Network;
+using System;
 
 namespace SharedFiles
 {
@@ -7,6 +9,24 @@ namespace SharedFiles
         static void Main(string[] args)
         {
             Console.WriteLine("This project is not for production");
+
+            ClientWrapper client = new ClientWrapper();
+            client.Connect("localhost", 54000);
+
+            if (client.Connected())
+            {
+                DataList data = client.ExecuteCommand("select from table1");
+                Console.WriteLine(data.items[0].ToString());
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine("Connection failed");
+            }
+
+            client.Disconnect();
+
+            Console.ReadKey();
         }
     }
 }
