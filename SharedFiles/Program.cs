f3d08953c39b78085f4f03e492b14f48e92df2e8
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-13 12:17:13 +0300

    Added more testing code

--- SharedFiles/Program.cs
+++ SharedFiles/Program.cs
@@ -16,7 +16,20 @@ namespace SharedFiles
             if (client.Connected())
             {
                 DataList data = client.ExecuteCommand("select from table1");
-                Console.WriteLine(data.items[0].ToString());
+                Console.WriteLine(data);
+
+                DataList row1 = new DataList();
+                row1.Add("sportas", "name");
+                row1.Add("miske", "location");
+                row1.Add("sportavimas", "sport");
+                row1.Add(4.20, "price");
+                DataList teams1 = new DataList();
+                teams1.Add("as");
+                teams1.Add("tu");
+                row1.Add(teams1, "teams");
+                client.AddEntry(row1, "table1");
+
+                data = client.ExecuteCommand("select from table1");
                 Console.WriteLine(data);
             }
             else
