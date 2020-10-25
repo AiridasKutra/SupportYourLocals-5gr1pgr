75cfe9b37e6ffd4e839a02f797024ffc342872af
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-25 13:19:56 +0200

    Fixed Entry editor

--- localhostUI/Backend/DataManagement/DatabaseEntryEditor.cs
+++ localhostUI/Backend/DataManagement/DatabaseEntryEditor.cs
@@ -8,15 +8,17 @@ namespace localhostUI.Backend.DataManagement
     class DatabaseEntryEditor : IDataWriter
     {
         private string tableName;
+        private int id;
 
-        public DatabaseEntryEditor(string tableName)
+        public DatabaseEntryEditor(string tableName, int id)
         {
             this.tableName = tableName;
+            this.id = id;
         }
 
         public void Write(DataList data)
         {
-            Program.Client.ModifyEntry(data, tableName, -1);
+            Program.Client.ModifyEntry(data, tableName, id);
         }
     }
 }
