92db6a11e5a21bb6f94cee8bda400c378c020b7a
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-25 13:19:46 +0200

    Added entry remover

---
+++ localhostUI/Backend/DataManagement/DatabaseEntryRemover.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace localhostUI.Backend.DataManagement
+{
+    class DatabaseEntryRemover : IDataWriter
+    {
+        private string tableName;
+        private int id;
+
+        public DatabaseEntryRemover(string tableName, int id)
+        {
+            this.tableName = tableName;
+            this.id = id;
+        }
+
+        public void Write(DataList data)
+        {
+            Program.Client.RemoveEntry(tableName, id);
+        }
+    }
+}
