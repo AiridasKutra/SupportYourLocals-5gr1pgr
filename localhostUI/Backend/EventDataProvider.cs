cc2fc230969f92b1269c2c6d23d2e93b65298fa0
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-26 14:31:07 +0200

    Made data loading from the database only occur once

--- localhostUI/Backend/EventDataProvider.cs
+++ localhostUI/Backend/EventDataProvider.cs
@@ -7,16 +7,22 @@ namespace localhostUI.Backend
 {
     class EventDataProvider
     {
-        public bool InitialLoadDone { get; private set; }
+        public bool InitialLoadDoneBrief { get; private set; }
+        public bool InitialLoadDoneFull { get; private set; }
 
         public EventDataProvider()
         {
-            InitialLoadDone = false;
+            InitialLoadDoneBrief = false;
+            InitialLoadDoneFull = false;
         }
 
         public List<EventBrief> GetEventsBrief(EventOptions options)
         {
-            Program.DataPool.LoadEventsBrief();
+            if (!InitialLoadDoneBrief)
+            {
+                Program.DataPool.LoadEventsBrief();
+                InitialLoadDoneBrief = true;
+            }
 
             List<EventBrief> events = new List<EventBrief>();
             foreach (var evBrief in Program.DataPool.eventsBrief)
@@ -31,7 +37,11 @@ namespace localhostUI.Backend
 
         public List<EventFull> GetEventsFull(EventOptions options)
         {
-            Program.DataPool.LoadEventsFull();
+            if (!InitialLoadDoneFull)
+            {
+                Program.DataPool.LoadEventsFull();
+                InitialLoadDoneFull = true;
+            }
 
             List<EventFull> events = new List<EventFull>();
             foreach (var evFull in Program.DataPool.eventsFull)
