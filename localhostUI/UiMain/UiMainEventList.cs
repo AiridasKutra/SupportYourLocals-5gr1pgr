314971f1a40e229532fb52f104f9dbbffc3e924a
Airidas Kutra <airidas.kutra@mif.stud.vu.lt.com>
2020-10-27 15:16:03 +0200

    Improved event look

--- localhostUI/UiMain/UiMainEventList.cs
+++ localhostUI/UiMain/UiMainEventList.cs
@@ -2,6 +2,9 @@
 using localhostUI.Classes.EventClasses;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
@@ -17,18 +20,48 @@ namespace localhostUI
             List<EventBrief> events = Program.DataProvider.GetEventsBrief(options);
 
             // Add all of them to a list
+            int col = 0;
+            int count = 0;
             foreach (var eBrief in events)
             {
                 // Main container
                 Panel eventPanel = new Panel();
-                eventPanel.AutoSize = true;
+                eventPanel.AutoSize = false;
+                eventPanel.Size = new Size(240, 238);
+                eventPanel.Margin = new Padding(8);
+                eventPanel.BorderStyle = BorderStyle.Fixed3D;
+
+                // Thumbnail
+                PictureBox thumbnail = new PictureBox();
+                thumbnail.Size = new Size(240, 180);
+                thumbnail.Location = new Point(0, 0);
+                try
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        Stream stream = client.OpenRead(eBrief.Thumbnail);
+                        Bitmap bitmap = new Bitmap(stream);
+                        Bitmap bitmapScaled = new Bitmap(bitmap, new Size(240, 180));
+                        thumbnail.Image = bitmapScaled;
+
+                        stream.Flush();
+                        stream.Close();
+                    }
+                }
+                catch
+                {
+
+                }
 
                 // Name label
                 Label eventName = new Label();
                 eventName.Text = eBrief.Name;
                 eventName.AutoSize = true;
-                eventName.Location = new System.Drawing.Point(0, 0);
-                eventName.Font = new System.Drawing.Font("Comic Sans MS", 15);
+                eventName.Location = new Point(0, 180);
+                eventName.MinimumSize = new Size(240, 30);
+                eventName.Font = new Font("Comic Sans MS", 12);
+                eventName.BackColor = Color.FromArgb(240, 240, 240);
+                eventName.TextAlign = ContentAlignment.MiddleLeft;
 
                 // Sport label
                 Label eventSports = new Label();
@@ -38,20 +71,27 @@ namespace localhostUI
                     eventSports.Text += $"{sport}  ";
 
                 }
-                eventSports.AutoSize = true;
-                eventSports.Location = new System.Drawing.Point(0, 30);
+                eventSports.AutoSize = false;
+                eventSports.Location = new Point(0, 210);
+                eventSports.Size = new Size(240, 25);
+                eventSports.BackColor = Color.FromArgb(230, 230, 230);
+                eventSports.TextAlign = ContentAlignment.MiddleLeft;
 
                 // Add everything
+                eventPanel.Controls.Add(thumbnail);
                 eventPanel.Controls.Add(eventName);
                 eventPanel.Controls.Add(eventSports);
 
-                CurrentEventsTable.Controls.Add(eventPanel);
+                CurrentEventsTable.Controls.Add(eventPanel, col, count / CurrentEventsTable.ColumnCount);
+
+                col = (++col) % CurrentEventsTable.ColumnCount;
+                count++;
 
                 // Redraw
                 Invalidate();
             }
 
-            CurrentEventsTable.RowCount = events.Count;
+            CurrentEventsTable.RowCount = (events.Count + 1) / CurrentEventsTable.ColumnCount;
         }
     }
 }
