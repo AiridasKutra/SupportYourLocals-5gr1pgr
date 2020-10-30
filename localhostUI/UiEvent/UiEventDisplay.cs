a3568ca290065802fbad64f4bf4c1142d375f1ca
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2020-10-30 23:27:42 +0200

    Revert "Fixed event address opening exception handling."
    This reverts commit d201282e5e71fda8ee219ee4cdee78992110f26c.

--- localhostUI/UiEvent/UiEventDisplay.cs
+++ localhostUI/UiEvent/UiEventDisplay.cs
@@ -97,15 +97,7 @@ namespace localhostUI.UiEvent
         private void ShowAddressButton_Click(object sender, EventArgs e)
         {
             AddressData data = LocationInformation.AddressFromLatLong(@event.Latitude, @event.Longitude);
-            try
-            {
-                LocationInformation.OpenAdressInBrowser(data.Address);
-            }
-            catch
-            {
-                MessageBox.Show("A mistake was made in opening the address.", "Something went wrong.",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            LocationInformation.OpenAdressInBrowser(data.Address);
         }
     }
 }
