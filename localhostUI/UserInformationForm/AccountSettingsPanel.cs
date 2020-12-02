fdcde1aca281e2dbceea9b0926b775901abcc1e7
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2020-12-02 20:41:40 +0200

    setong

---
+++ localhostUI/UserInformationForm/AccountSettingsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace localhostUI.UserInformationForm
+{
+    public partial class AccountSettingsPanel : Form
+    {
+        /*
+         * change password
+         * change address
+         * 
+         * delete account
+         */
+        public AccountSettingsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ localhostUI/UserInformationForm/AppSettingsPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace localhostUI.UserInformationForm
+{
+    public partial class AppSettingsPanel : Form
+    {
+        /*
+         * Dark mode
+         * Auto log-in
+         * 
+         */
+        public AppSettingsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
