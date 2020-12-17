8af008122c0fd4d4dbd1b760567cf36a47fb4888
zjuci <zygimantas.jucius@mif.stud.vu.lt>
2020-12-17 22:01:41 +0200

    Created mobile app

---
+++ MobileApp/MobileApp.Android/MainApplication.cs
@@ -0,0 +1,22 @@
+﻿#if DEBUG
+using Android.App;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using System;
+
+[Application(Debuggable = true)]
+#else
+    [Application(Debuggable = false)]
+#endif
+    [MetaData("com.google.android.maps.v2.API_KEY", Value = "")]
+public class MainApplication : Application
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+    { }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        CrossCurrentActivity.Current.Init(this);
+    }
+}
---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Pin mifPin = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(54.729729, 25.263571),
+                Label = "MIFAS",
+                Address = "Didlaukio bbz kiek",
+                Tag = "id_Mif"
+            };
+
+            map.Pins.Add(mifPin);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(mifPin.Position, Distance.FromMeters(500)));
+        }
+    }
+}
