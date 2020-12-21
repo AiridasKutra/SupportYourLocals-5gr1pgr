fe34c1bbc42cdec80e4adc71c5a5850d5c78bdc5
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2020-12-21 16:47:25 +0200

    Added a button with animations and shit, real cool stuff

--- MobileAndroidApp/MainActivity.cs
+++ MobileAndroidApp/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Android.Animation;
 using Android.App;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
@@ -15,14 +16,114 @@ namespace MobileAndroidApp
     public class MainActivity : AppCompatActivity, IOnMapReadyCallback
     {
         private GoogleMap map;
+        private static bool isFabOpen = false;
+        private FloatingActionButton fabMain;
+        private FloatingActionButton fabEvents;
+        private FloatingActionButton fabAccount;
+        private FloatingActionButton fabSettings;
+        private FloatingActionButton fabLogin;
+        private View bgFabMenu;
+
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
             SetContentView(Resource.Layout.activity_main);
-
+            fabMain = FindViewById<FloatingActionButton>(Resource.Id.fab_main);
+            fabEvents = FindViewById<FloatingActionButton>(Resource.Id.fab_events);
+            fabAccount = FindViewById<FloatingActionButton>(Resource.Id.fab_account_manager);
+            fabSettings = FindViewById<FloatingActionButton>(Resource.Id.fab_settings);
+            fabLogin = FindViewById<FloatingActionButton>(Resource.Id.fab_login);
+            bgFabMenu = FindViewById<View>(Resource.Id.bg_fab_menu);
             SetUpMap();
+
+            fabMain.Click += (o, e) =>
+             {
+                 if (!isFabOpen) ShowFabMenu();
+                 else CloseFabMenu();
+             };
+
+            bgFabMenu.Click += (o, e) => CloseFabMenu();
+        }
+
+        private void CloseFabMenu()
+        {
+            isFabOpen = false;
+            fabMain.Animate().Rotation(0f);
+            bgFabMenu.Animate().Alpha(0f);
+
+            fabEvents.Animate()
+                .TranslationY(0)
+                .Rotation(90f);
+            fabAccount.Animate()
+                .TranslationY(0)
+                .Rotation(90f);
+            fabSettings.Animate()
+                .TranslationY(0)
+                .Rotation(90f);
+            fabLogin.Animate()
+                .TranslationY(0)
+                .Rotation(90f).SetListener(new FabAnimatorListener(bgFabMenu,fabEvents,fabAccount,fabSettings,fabLogin));
+        }
+        private class FabAnimatorListener : Java.Lang.Object, Animator.IAnimatorListener
+        {
+            View[] viewsToHide;
+            public FabAnimatorListener(params View[] viewsToHide)
+            {
+                this.viewsToHide = viewsToHide;
+            }
+            public void OnAnimationCancel(Animator animation)
+            {
+
+            }
+
+            public void OnAnimationEnd(Animator animation)
+            {
+                if (!isFabOpen)
+                {
+                    foreach(var view in viewsToHide)
+                    {
+                        view.Visibility = ViewStates.Gone;
+                    }
+                }
+            }
+
+            public void OnAnimationRepeat(Animator animation)
+            {
+
+            }
+
+            public void OnAnimationStart(Animator animation)
+            {
+
+            }
+        }
+        private void ShowFabMenu()
+        {
+            isFabOpen = true;
+            fabEvents.Visibility = ViewStates.Visible;
+            fabAccount.Visibility = ViewStates.Visible;
+            fabSettings.Visibility = ViewStates.Visible;
+            fabLogin.Visibility = ViewStates.Visible;
+            bgFabMenu.Visibility = ViewStates.Visible;
+
+            fabMain.Animate().Rotation(-90f);
+            bgFabMenu.Animate().Alpha(1f);
+
+            fabEvents.Animate()
+                .TranslationY(Resources.GetDimension(Resource.Dimension.standart_170))
+                .Rotation(0f);
+            fabAccount.Animate()
+                .TranslationY(Resources.GetDimension(Resource.Dimension.standart_130))
+                .Rotation(0f);
+            fabSettings.Animate()
+                .TranslationY(Resources.GetDimension(Resource.Dimension.standart_90))
+                .Rotation(0f);
+            fabLogin.Animate()
+                .TranslationY(Resources.GetDimension(Resource.Dimension.standart_50))
+                .Rotation(0f);
         }
 
         private void SetUpMap()
--- MobileApp/MobileApp.Android/MainActivity.cs
+++ MobileApp/MobileApp.Android/MainActivity.cs
@@ -7,12 +7,21 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Plugin.CurrentActivity;
+using Android.Support.Design.Widget;
 
 namespace MobileApp.Droid
 {
     [Activity(Label = "MobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private static bool isFabOpen;
+        private FloatingActionButton fabMain;
+        private FloatingActionButton fabEvents;
+        private FloatingActionButton fabAccount;
+        private FloatingActionButton fabSettings;
+        private FloatingActionButton fabLogin;
+        private View bgFabMenu;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -27,6 +36,9 @@ namespace MobileApp.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+
+
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
