ad643edee97e4862fc07cbbaec86a056ca5190d4
Gytis Oksas <gytis.oksas@mif.stud.vu.lt>
2020-12-23 20:38:56 +0200

    Added sign up page, minor esthetic changes

--- MobileAndroidApp/ActivityControllers/LoginActivity.cs
+++ MobileAndroidApp/ActivityControllers/LoginActivity.cs
@@ -10,6 +10,7 @@ namespace localhost.ActivityControllers
     public class LoginActivity : Activity
     {
         Button loginButton;
+        Button signupButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -17,12 +18,21 @@ namespace localhost.ActivityControllers
             // Create your application here
             loginButton = FindViewById<Button>(Resource.Id.loginButton);
             loginButton.Click += LogIn;
+            signupButton = FindViewById<Button>(Resource.Id.signUpButton);
+            signupButton.Click += SignUp;
         }
 
         public void LogIn(object o, EventArgs e)
         {
             MainActivity.IsLoggedIn = true;
             Toast.MakeText(this,"Logged in", ToastLength.Short).Show();
+            Finish();
+        }
+
+        public void SignUp(object o, EventArgs e)
+        {
+            
+            StartActivity(typeof(SignUpActivity));
             Finish();
         }
     }
